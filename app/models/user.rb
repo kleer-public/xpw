71b5ddbf4f38c6b73b2999a145f7a7da5299e749
NicoPaez <nicopaez@gmail.com>
2013-08-02 09:24:41 -0300

    Validation hook added

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,11 @@ class User
   validates_presence_of :crypted_password
   validates_presence_of :email
   validates_format_of   :email,    :with => :email_address
+  validates_with_method :is_secure_password?
+
+  def is_secure_password?
+    true
+  end
 
   def password= (password)
     self.crypted_password = ::BCrypt::Password.create(password) unless password.nil?	
